7a9dedf61e58b2b6e8b90fefd67ad447b5529a5b
quarzasiphix <quarrysiphix@gmail.com>
2023-03-27 13:18:59 +0100

    item remove and add works

--- quarza-vite/src/views/WorkoutBot.tsx
+++ quarza-vite/src/views/WorkoutBot.tsx
@@ -1,6 +1,6 @@
-import { useState, useEffect } from "react";
+import { useEffect, useState } from "react";
 import useFetch, { ApiResponse } from "../useFetch"
-
+import './styles/workout.css'
 
 interface Workout {
     id: number;
@@ -11,57 +11,171 @@ interface Workout {
     updated_at: string;
 }
 
-const WorkoutReminder = () => {
-    const [workouts, setWorkouts] = useState<Workout[]>([]);
-    const [error, setError] = useState<Error | null>(null);
-    const [fetched, setFetched] = useState(false);
-    const [fetchKey, setFetchKey] = useState(0);
+interface WorkoutItem {
+    time: number;
+    workout: string;
+    todo: string;
+}
 
-    const getData = async () => {
-        const { data, pending, error } = await useFetch<Workout[]>(
-          "https://api.quarza.online/api/workouts"
-        );
+function AddWorkout() {
+    const [time, setTime] = useState<number>(6);
+    const [workout, setWorkout] = useState<string>("");
+    const [todo, setTodo] = useState<string>("");
+    const [responseMessage, setResponseMessage] = useState<string | null>(null);
+    const [errorMessage, setErrorMessage] = useState<string | null>(null);
 
-        if (!pending && !error) {
-          setWorkouts(data || []);
-          setError(null);
-        } else if (error) {
-          setError(error);
-          setWorkouts([]);
-        }
 
-        setFetched(true);
+    const handleTimeChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        const time = parseInt(event.target.value);
+        setTime(time);
     };
 
-    useEffect(() => {
-      getData();
-    }, [fetchKey]);
+    const handleWorkoutChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setWorkout(event.target.value);
+    };
 
-    const refreshData = () => {
-      setWorkouts([]);
-      setError(null);
-      setFetchKey(prevKey => prevKey + 1);
+    const handleTodoChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setTodo(event.target.value);
+    };
+
+    const addItem = () => {
+        const Items: WorkoutItem = { time, workout, todo };
+        console.log(JSON.stringify({ workout: Items }))
+        fetch("https://api.quarza.online/api/workout/store", {
+            method: "POST",
+            headers: {
+            "Content-Type": "application/json"
+            },
+            body: JSON.stringify({ workout: Items }),
+        })
+        .then(response => {
+            console.log(response);
+            setResponseMessage(`Item added successfully.`);
+            setErrorMessage(null);
+        })
+        .catch(error => {
+            console.error(error);
+            setResponseMessage(null);
+            setErrorMessage(`Error adding item: ${error}`);
+        });
+    }
+
+    const handleButtonClick = () => {
+        addItem();
     };
 
     return (
-      <div>
-        {error && <p>{error.message}</p>}
-        {workouts.length > 0 ? (
-          <ul>
-            {workouts.map((workout) => (
-              <li key={workout.id}>
-                time: {workout.time}
-                {workout.workout}
-                {workout.todo}
-              </li>
-            ))}
-          </ul>
-        ) : (
-          <p>{fetched ? "No workouts found" : "Loading..."}</p>
-        )}
-        <button onClick={refreshData}>Refresh</button>
+      <div className="add">
+        <label htmlFor="time-input">Time:</label>
+        <input
+            id="time-input"
+            type="number"
+            value={time}
+            onChange={handleTimeChange}
+            min={6}
+            max={20}
+        />
+        <br />
+        <label htmlFor="workout-input">Workout:</label>
+        <input
+            id="workout-input"
+            type="text"
+            value={workout}
+            onChange={handleWorkoutChange}
+            />
+        <br />
+        <label htmlFor="todo-input">Todo:</label>
+        <input
+            id="todo-input"
+            type="text"
+            value={todo}
+            onChange={handleTodoChange}
+            />
+        <br />
+        <button onClick={handleButtonClick}>Add item</button>
+        {responseMessage && <p>{responseMessage}</p>}
+        {errorMessage && <p>{errorMessage}</p>}
       </div>
     );
 };
 
-export default WorkoutReminder;
+const WorkoutReminder = () => {
+
+    const { data, pending, error } = useFetch<Workout[]>(
+        "https://api.quarza.online/api/workouts"
+    );
+
+    if (pending) {
+        return <p>Loading...</p>;
+    }
+
+    if (error) {
+        return <p>Error: {error.message}</p>;
+    }
+
+    if (!data) {
+        return null;
+    }
+
+    const RemoveItem = () => {
+        const [time, setTime] = useState<number>(6);
+        const [responseMessage, setResponseMessage] = useState<string | null>(null);
+        const [errorMessage, setErrorMessage] = useState<string | null>(null);
+
+        const handleTimeChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+            const time = parseInt(event.target.value);
+            setTime(time);
+        };
+        const Remove = () => {
+            console.log("removing item ", time)
+            const url = `https://api.quarza.online/api/workout/${time}`;
+
+            fetch(url, {
+                method: "DELETE",
+            })
+            .then(response => {
+                console.log(response);
+                setResponseMessage(`Item removed successfully.`);
+                setErrorMessage(null);
+            })
+            .catch(error => {
+                console.error(error);
+                setResponseMessage(null);
+                setErrorMessage(`Error removing item: ${error}`);
+            });
+
+        }
+
+        return (
+            <div className="remove">
+                <p> Time: </p>
+                <input type="text" onChange={handleTimeChange} />
+                <button onClick={Remove}> remove </button>
+                {responseMessage && <p>{responseMessage}</p>}
+                {errorMessage && <p>{errorMessage}</p>}
+            </div>
+        );
+    }
+
+    return (<>
+    <div className="secs">
+        <AddWorkout/>
+        <RemoveItem/>
+    </div>
+    <div className="sec">
+        <div className="workout-preview">
+        <ul>
+        {data.map((workout) => (
+            <li key={workout.id}>
+            <h4>{workout.workout}</h4>
+            <p>time: {workout.time}</p>
+            <p>{workout.todo}</p>
+            </li>
+        ))}
+        </ul>
+        </div>
+    </div>
+    </>);
+}
+
+export default WorkoutReminder
